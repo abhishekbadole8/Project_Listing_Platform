021ce6fb712dc834eb86ad35abb7a18dcd13f9f7
Abhishek Badole <abhishekbadole8@gmail.com>
2023-07-06 16:39:59 +0530

    api address bug clear

--- src/components/apiClient/apiClient.js
+++ src/components/apiClient/apiClient.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:5000`,
-  // baseURL: `https://project-listing-platform.onrender.com`,
+   baseURL: `https://project-listing-platform.onrender.com`,
 });
 
 export default apiClient;
